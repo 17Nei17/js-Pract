0d0c8082445ebbf7e1461fde56bb7ff10219ad67
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-07-16 15:42:51 +0300

    Add files via upload

---
+++ js-lecture-1-master/index.js
@@ -0,0 +1,18 @@
+// index.js
+//console.log('hello world!');
+const http = require ('http');
+const fs = require ('fs');
+
+const server = http.createServer(function (request, responce) {
+    console.log(request.method, request.url);
+    if (request.url == '/'){
+       const content = fs.readFileSync ('index.html','utf8'); 
+       responce.end(content); 
+    }
+    else if (request.url == '/style.css'){
+        const content = fs.readFileSync ('style.css','utf8'); 
+        responce.end(content); 
+    }
+})
+server.listen(process.env.PORT || 3000 );
+console.log('server started');
