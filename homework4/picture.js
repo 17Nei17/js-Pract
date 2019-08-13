6c38f34d1cd899f13cac88fe6f3907d341ed5d5f
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-08-13 18:32:20 +0300

    Add files via upload

--- homework4/picture.js
+++ homework4/picture.js
@@ -1,20 +1,20 @@
 
 function PictureLoad (method,data,headers) {
     fetch ('"https://intern-staging.herokuapp.com/api/file' , {
-        method: method,
-        body: data,
+        method:method,
+        body:data,
         headers:headers,  
     }).then(
-        resp=>resp.json()
-        ).then(
-            json =>  console.log (json) ,
-        );
+        resp => resp.json()
+    ).then(
+        json => console.log(json),
+    );
 }
-var forme = document.forms.namedItem ('fileinfo') ;
+var forme = document.forms.namedItem('fileinfo') ;
     forme.addEventListener ('submit' , function(ev) {
     var formData = new FormData(this);
     formData.append('parentEntityId', '5d35ef5608d39b00048a139a');
     var tokenStr=localStorage.getItem('token');
-    PictureLoad('POST',formData, {'token':tokenStr});
+    PictureLoad('POST',formData, {'token' : tokenStr});
    ev.preventDefault();
 })
